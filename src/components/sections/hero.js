083a19e2af0086a12d238f44a4170f2751c2a19b
Lycanpire <32617328+Lycanpire@users.noreply.github.com>
2023-05-22 17:18:31 +0530

    1.1

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -67,7 +67,7 @@ const Hero = () => {
       <p>
       Syed Akbar Abbas Jafri is an MBA graduate from XIMB with expertise in full stack software development and consulting, honed through his experience at Cognizant.{' '}
         <a href="https://www.instagram.com/the_guy_is_no_one/" target="_blank" rel="noreferrer">
-          Wanna catch up on his casual side!
+          Wanna catch up on his casual side?
         </a>
         .
       </p>
