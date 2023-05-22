392886023048fc7943e2c1cd568eac4cee575a63
Lycanpire <32617328+Lycanpire@users.noreply.github.com>
2023-05-22 14:03:04 +0530

    Final build tow web

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,7 +4,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": preferDefault(require("/Users/Akbar/Documents/My-Portfolio/.cache/caches/gatsby-plugin-offline/app-shell.js")),
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/Akbar/Documents/My-Portfolio/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/Akbar/Documents/My-Portfolio/src/pages/404.js")),
   "component---src-pages-archive-js": preferDefault(require("/Users/Akbar/Documents/My-Portfolio/src/pages/archive.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/Akbar/Documents/My-Portfolio/src/pages/index.js")),
